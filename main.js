5cf1f2e41a602c928920180dcb320590f251b82e
UNIVERSE\bpukhashvili <beka.pukhashvili@dataart.com>
2023-01-16 13:46:30 +0400

    added bombs

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 const widthInput = document.getElementById("width");
 const heightInput = document.getElementById("height");
 const mineField = document.querySelector(".button-grid");
+let bombNumber = (widthInput.value * heightInput.value) / 10;
+let bombs = [];
+let width = 10;
+let height = 10;
 
 function resetForm() {
     widthInput.value = 10;
     heightInput.value = 10;
 }
 
+function setBombNumber(width, height) {
+    return (width * height) / 10;
+}
+
 function generateTable(width, height) {
+    setBombs(width, height);
+
     document.querySelector(".button-grid").innerHTML = "";
     for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
@@ -21,17 +31,38 @@ function generateTable(width, height) {
     resetForm();
 }
 
+function setBombs(width, height) {
+    bombs = [];
+    bombNumber = setBombNumber(width, height);
+    while (bombs.length < bombNumber) {
+        const x = getRandom(width);
+        const y = getRandom(height);
+
+        const itemInArray = bombs.find(bomb => bomb.x == x && bomb.y == y);
+        if (itemInArray == null) {
+            bombs.push({ x, y });
+        }
+    }
+    console.log(bombs);
+}
+
+function getRandom(n) {
+    return Math.floor(Math.random() * n);
+}
+
 function mark(x, y) {
     const markedButton = document.querySelector(`[x="${x}"][y="${y}"]`);
-    if (markedButton.style.backgroundColor == "green") {
+    if (markedButton.style.backgroundColor == "blue") {
         markedButton.style.backgroundColor = "red";
         return;
     }
     if (markedButton.style.backgroundColor == "") {
-        markedButton.style.backgroundColor = "green";
+        markedButton.style.backgroundColor = "blue";
+        return;
     }
     if (markedButton.style.backgroundColor == "red") {
         markedButton.style.backgroundColor = "";
+        return;
     }
 }
 
@@ -40,11 +71,7 @@ document.querySelector(".button-grid").addEventListener("contextmenu", (e) => {
     mark(e.target.getAttribute("x"), e.target.getAttribute("y"));
 });
 
-let width = 10;
-let height = 10;
-
 document.getElementById("props").addEventListener("click", () => {
-    console.log("click");
     width = widthInput.value;
     if (width > 20) width = 20;
     height = heightInput.value;
